a6370a357e85495aedae30d84fc4072f54fbcd71
mary <maryantaneeta@gmail.com>
2021-10-25 23:13:34 +0530

    primitive datatypes

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("Hello World!")
+console.warn("Warning")
+console.error("Error")
+
+//string
+console.log(typeof "Hello World!")
+console.log(typeof 's')
+
+//number
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 125467867554)
+console.log(typeof -1)
+console.log(typeof -1.2)
+
+//boolean
+console.log(typeof true)
+console.log(typeof false)
