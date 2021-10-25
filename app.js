abcd2d55a8a6f07a54f0ff6924afe3626dc204f5
mary <maryantaneeta@gmail.com>
2021-10-25 23:27:47 +0530

    null vs undefined

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-console.log("Hello World!")
-console.warn("Warning")
-console.error("Error")
-
-//string
-console.log(typeof "Hello World!")
-console.log(typeof 's')
-
-//number
-console.log(typeof 1)
-console.log(typeof 1.2)
-console.log(typeof 125467867554)
-console.log(typeof -1)
-console.log(typeof -1.2)
-
-//boolean
-console.log(typeof true)
-console.log(typeof false)
+var age =  null
+console.log(age)
+console.log(typeof age)
+var Name
+console.log(Name)
