15f6a101cab698db9124ba2161a3c5d66b076855
mary <maryantaneeta@gmail.com>
2021-10-26 00:05:57 +0530

    operators

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
-var age =  null
-console.log(age)
-console.log(typeof age)
-var Name
-console.log(Name)
+//operators
+
+//arithmetic operators
+a = 2
+b = 10
+console.log("a+b=", a + b )
+console.log("b-a=", b-a )
+console.log("b/a=", b/a )
+console.log("a*b=", a * b )
+console.log("b**a=", b**a )
+console.log("b%a=", b%a )
+
+//assignment operators
+console.log('b', b)
+b+=10
+console.log('b+=10',b)
+b++
+console.log('b++',b)
+b-=10
+console.log('b-=10',b)
+b--
+console.log('b--',b)
+
+var c = 10
+console.warn(c++)
+console.warn(c)
+
+//comparison operator
+var d=10
+var e=2
+var f='10'
+console.log(d>e)
+console.log(d>e)
+console.log(d<=e)
+console.log(d>=e)
+console.log(d==e)
+console.warn(d==f)
+console.warn(d===f)
+
+// logical operators
+console.log('true && true',true && true)
+console.log('true && false',true && false)
+console.log('false && true', false && true)
+console.log('false && false', false && false)
+
+console.log('true || true',true || true)
+console.log('true || false',true || false)
+console.log('false || true', false || true)
+console.log('false || false', false || false)
